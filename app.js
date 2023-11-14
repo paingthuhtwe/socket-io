b2e66e7c75b88a4eb782de46eb0ab161b221d349
Paing Thu Htwe <paingthuhtwe.official@gmail.com>
2023-11-15 02:00:29 +0630

    fix in domain

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/', (req, res, next) => {
 })
 
 // Start the server
-const PORT = 8000;
+const PORT = 4444;
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 })
